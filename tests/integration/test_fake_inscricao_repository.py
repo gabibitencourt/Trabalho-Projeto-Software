555074b49ff85903e5dd027c6b83168d7fbd868f
gabibitencourt <gabrielabfs@id.uff.br>
2026-09-24 21:35:38 -0300

    fix: removi listar_por_evento dos testes pois decidi tirar essa função do repositorio

--- tests/integration/test_fake_inscricao_repository.py
+++ tests/integration/test_fake_inscricao_repository.py
@@ -2,10 +2,8 @@ import pytest
 from eventos.domain.model import Inscricao
 from eventos.adapters.repository import FakeInscricaoRepository
 
-def criar_inscricao(identificador=1, participante="Gabriela", lote="Lote 1", evento_id="EVT-01"):
-    inscricao = Inscricao(identificador=identificador, participante=participante, lote=lote)
-    inscricao.evento = evento_id 
-    return inscricao
+def criar_inscricao(identificador=1, participante="Gabriela", lote="Lote 1"):
+    return Inscricao(identificador=identificador, participante=participante, lote=lote)
 
 def test_repository_pode_adicionar_e_obter_inscricao():
     repo = FakeInscricaoRepository()
@@ -32,17 +30,6 @@ def test_repository_pode_listar_inscricoes_por_participante():
     
     assert len(resultados) == 2
     assert all(i.participante == "Gabriela" for i in resultados)
-
-def test_repository_pode_listar_inscricoes_por_evento():
-    repo = FakeInscricaoRepository()
-    repo.adicionar(criar_inscricao(identificador=1, evento_id="PythonBrasil"))
-    repo.adicionar(criar_inscricao(identificador=2, evento_id="PythonBrasil"))
-    repo.adicionar(criar_inscricao(identificador=3, evento_id="CampusParty"))
-    
-    resultados = repo.listar_por_evento("PythonBrasil")
-    
-    assert len(resultados) == 2
-    assert all(i.evento == "PythonBrasil" for i in resultados)
 
 def test_repository_pode_atualizar_inscricao():
     repo = FakeInscricaoRepository()
